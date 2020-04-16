8edd81e17ea144838448b41caa93e12d5ff8659c
Karen Ye <khye@wustl.edu>
2020-04-16 04:31:42 -0500

    select location

--- CSE237-FinalProject/src/destination/Destination.java
+++ CSE237-FinalProject/src/destination/Destination.java
@@ -19,49 +19,4 @@ public class Destination {
 	public String getLocation() {
 		return this.location.getLocation();
 	}
-	
-	public Set<User> getTravelPeers() {
-		return this.travelPeers;
-	}
-	
-	public Map<User, Set<Landmark>> getLandmarks() {
-		return this.landMarks;
-	}
-	
-	public void addPeer(User user) {
-		this.travelPeers.add(user);
-	}
-	
-	public void removePeer(User user) {
-		this.travelPeers.remove(user);
-	}
-	
-	public int getTotalNumberOfPeers() {
-		return travelPeers.size();
-	}
-	
-	public void seeAllPeers() {
-		for(User user : travelPeers) {
-			System.out.println(user.getUser());
-		}
-	}
-	
-	
-	public void addLandmark(User user, String name) {
-		landMarks.get(user).add(new Landmark(name));
-	}
-	
-	public void removeLandmark(User user, String name) {
-		landMarks.get(user).remove(new Landmark(name));
-	}
-	
-	public int getTotalNumberOfLandmarks(User user) {
-		return landMarks.get(user).size();
-	}
-	
-	public void seeAllLandmarks(User user) {
-		for(Landmark landmark : landMarks.get(user)) {
-			System.out.println(landmark.getName());
-		}
-	}
 }
